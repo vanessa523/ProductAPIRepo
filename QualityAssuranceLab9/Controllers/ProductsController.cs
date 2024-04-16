080662fcc78a7744800bbb52daf8cce4bff0f633
vanessa523 <tun32988@temple.edu>
2024-04-16 14:01:26 -0400

    New Lab12 changes

--- QualityAssuranceLab9/Controllers/ProductsController.cs
+++ QualityAssuranceLab9/Controllers/ProductsController.cs
@@ -12,9 +12,9 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Potato Soup", Category = "Grocery", Price = 3 },
-            new Product { Id = 2, Name = "Doll", Category = "Toy", Price = 40 },
-            new Product { Id = 3, Name = "Wrench", Category = "Hardwares", Price = 27 }
+            new Product { Id = 1, Name = "Potato Soup", Category = "Grocery", Price = 12 },
+            new Product { Id = 2, Name = "Doll", Category = "Toy", Price = 3 },
+            new Product { Id = 3, Name = "Wrench", Category = "Tools", Price = 15 }
         };
 
         public ProductsController()
